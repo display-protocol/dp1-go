3eb20f83babedc376ef6277be999be3a598ea060
Sean Moss-Pultz <sean@moss-pultz.com>
2026-09-07 19:30:01 -0700

    feat: expose lightweight content rating validation

--- extension/contentrating/contentrating.go
+++ extension/contentrating/contentrating.go
@@ -1,6 +1,8 @@
 // Package contentrating defines the draft DP-1 content-rating extension types.
 package contentrating
 
+import "github.com/display-protocol/dp1-go/internal/validate"
+
 // Rating is a curatorial audience label. Its absence on a playlist item means unrated.
 type Rating string
 
@@ -8,3 +10,9 @@ const (
 	RatingGeneral Rating = "general"
 	RatingMature  Rating = "mature"
 )
+
+// ValidatePlaylistFragment validates only content-rating extension fields.
+// It intentionally does not require core fields or signatures.
+func ValidatePlaylistFragment(data []byte) error {
+	return validate.ContentRatingExtensionFragment(data)
+}
